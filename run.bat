75555d3979703dfde6217ebbb07196d06c0a2ea0
yushlaw <yushlaw@hotmail.com>
2020-09-07 20:49:27 -0400

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,1 +1,3 @@
-docker run --rm --name iku -e LICENSE=/license.json -p 8083:10000 -v %cd%\data:/home/dataiku/dss -v %cd%\config\license.json:/license.json -d iku
+mkdir data
+docker rm iku
+docker run --name iku -e LICENSE=/license.json -p 8082:10000 -v %cd%\data:/home/dataiku/dss -v %cd%\config\license.json:/license.json -v %cd%\jdbc:/home/dataiku/dss/lib/jdbc -d iku
